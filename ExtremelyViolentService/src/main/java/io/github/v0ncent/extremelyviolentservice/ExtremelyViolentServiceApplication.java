9675b10e57a82713b5bd6202a55a91d62dc7cade
v0ncent <83373495+v0ncent@users.noreply.github.com>
2025-06-03 22:22:51 -0500

    initial commit

---
+++ ExtremelyViolentService/src/main/java/io/github/v0ncent/extremelyviolentservice/ExtremelyViolentServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.v0ncent.extremelyviolentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtremelyViolentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtremelyViolentServiceApplication.class, args);
+    }
+
+}
---
+++ ExtremelyViolentService/src/test/java/io/github/v0ncent/extremelyviolentservice/ExtremelyViolentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.v0ncent.extremelyviolentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtremelyViolentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
