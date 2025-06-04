069ce08afeba38d08f0e9c35f45c14533ad38d6a
v0ncent <83373495+v0ncent@users.noreply.github.com>
2025-06-04 00:40:45 -0500

    Gallery portion of backend

--- ExtremelyViolentService/src/main/java/io/github/v0ncent/extremelyviolentservice/APIRoutes/GalleryController.java
+++ ExtremelyViolentService/src/main/java/io/github/v0ncent/extremelyviolentservice/APIRoutes/GalleryController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Optional;
 
 @RestController
